f51bb2e3f599a98d6f8134377fe67c7011f8f80c
Shahria Abeed <105658920+abeeds@users.noreply.github.com>
2023-01-19 21:00:14 -0500

    Update shop.h

--- shop.h
+++ shop.h
@@ -10,14 +10,25 @@ class Book;
 
 class Shop {
 public:
-    void addBook(); // employee can add copies of a book.
+    void addBookQuantity(); // employee can add copies of a book.
     void updateBook(); //  update prices/quantities
 
-    void login(); // enter as employee or guest
+    // enter as employee or guest
+    // 0: exit has been entered during login -> call this function again
+    // 1: logging in as guest
+    // 2: logging in as customer
+    // 3: logging in as employee
+    // 4: shut down 
+    int login(); // exit cannot be a valid user or pass
+
+
+
+    void addEmployee(); // can only be done by an employee
 
     void purchaseBook(); 
-
     void printLibrary(); // displays books available and corresponding data
+
+    void adminDisplay(); // displays all books 
 
     void saveData(); // saves information to a text file
     void retrieveData(); // checks for existing save data
@@ -26,6 +37,7 @@ public:
 private:
     std::unordered_map<int, Book> Library; // the int is the ID
     std::unordered_map<std::string, std::string> Employees; // username, password
+    std::unordered_map<std::string, std::string> Customers; // username, password
     int quantity;   // amount of unique books
     int revenue;    // money made
     std::string cur_mode; // employee or guest
